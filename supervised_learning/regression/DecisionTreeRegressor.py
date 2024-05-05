b1e34516cd824aef0eac1149e44347bcd80d2695
bouhadiomar03@gmail.com <bouhadiomar03@gmail.com>
2024-05-05 23:09:16 +0000

    DecisionTreeRegressor implementation + test file

---
+++ supervised_learning/regression/DecisionTreeRegressor.py
@@ -0,0 +1,77 @@
+import numpy as np
+from supervised_learning.BaseEstimator import BaseEstimator
+
+class Node:
+    def __init__(self, feature_index=None, threshold=None, left=None, right=None, var_red=None, value=None):
+        self.feature_index = feature_index
+        self.threshold = threshold
+        self.left = left
+        self.right = right
+        self.var_red = var_red
+        self.value = value
+
+class DecisionTreeRegressor(BaseEstimator):
+    def __init__(self, min_samples_split=2, max_depth=None):
+        self.root = None
+        self.min_samples_split = min_samples_split
+        self.max_depth = max_depth
+
+    def _build_tree(self, dataset, depth=0):
+        X, Y = dataset[:,:-1], dataset[:,-1]
+        num_samples, num_features = np.shape(X)
+        
+        if num_samples >= self.min_samples_split and (self.max_depth is None or depth <= self.max_depth):
+            best_split = self._get_best_split(dataset, num_samples, num_features)
+            if best_split["var_red"] > 0:
+                left_subtree = self._build_tree(best_split["dataset_left"], depth+1)
+                right_subtree = self._build_tree(best_split["dataset_right"], depth+1)
+                return Node(best_split["feature_index"], best_split["threshold"], left_subtree, right_subtree, best_split["var_red"])
+        
+        leaf_value = self._calculate_leaf_value(Y)
+        return Node(value=leaf_value)
+
+    def _get_best_split(self, dataset, num_samples, num_features):
+        best_split = {}
+        max_var_red = -float("inf")
+        
+        for feature_index in range(num_features):
+            feature_values = dataset[:, feature_index]
+            possible_thresholds = np.unique(feature_values)
+            for threshold in possible_thresholds:
+                dataset_left, dataset_right = self._split(dataset, feature_index, threshold)
+                if len(dataset_left) > 0 and len(dataset_right) > 0:
+                    y, left_y, right_y = dataset[:, -1], dataset_left[:, -1], dataset_right[:, -1]
+                    curr_var_red = self._variance_reduction(y, left_y, right_y)
+                    if curr_var_red > max_var_red:
+                        best_split = {"feature_index": feature_index, "threshold": threshold, "dataset_left": dataset_left, "dataset_right": dataset_right, "var_red": curr_var_red}
+                        max_var_red = curr_var_red
+        return best_split
+
+    def _split(self, dataset, feature_index, threshold):
+        dataset_left = dataset[dataset[:, feature_index] <= threshold]
+        dataset_right = dataset[dataset[:, feature_index] > threshold]
+        return dataset_left, dataset_right
+
+    def _variance_reduction(self, parent, l_child, r_child):
+        weight_l = len(l_child) / len(parent)
+        weight_r = len(r_child) / len(parent)
+        return np.var(parent) - (weight_l * np.var(l_child) + weight_r * np.var(r_child))
+
+    def _calculate_leaf_value(self, Y):
+        return np.mean(Y)
+
+    def fit(self, X, Y):
+        dataset = np.concatenate((X, Y.reshape(-1, 1)), axis=1)
+        self.root = self._build_tree(dataset)
+
+    def _make_prediction(self, x, tree):
+        if tree.value is not None:
+            return tree.value
+        feature_val = x[tree.feature_index]
+        if feature_val <= tree.threshold:
+            return self._make_prediction(x, tree.left)
+        else:
+            return self._make_prediction(x, tree.right)
+
+    def predict(self, X):
+        return np.array([self._make_prediction(x, self.root) for x in X])
