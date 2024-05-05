286d61862769b74eb8c15d432a09cc374e7be080
Basma-Arnaoui <basma.arnaoui@gmail.com>
2024-05-05 20:53:05 +0100

    working logistic regression

--- supervised_learning/classification/LogisticRegression.py
+++ supervised_learning/classification/LogisticRegression.py
@@ -1,9 +1,9 @@
 import numpy as np
-from scipy.special import expit  # Sigmoid function
+from scipy.special import expit
 from supervised_learning.BaseEstimator import BaseEstimator
 
 class LogisticRegression(BaseEstimator):
-    def __init__(self, learning_rate=0.001, n_iterations=5000, tol=0.000001, regularization=0.01):
+    def __init__(self, learning_rate=0.01, n_iterations=1000, tol=1e-5, regularization=0.01):
         self.learning_rate = learning_rate
         self.n_iterations = n_iterations
         self.tol = tol
@@ -15,32 +15,31 @@ class LogisticRegression(BaseEstimator):
         return expit(z)
 
     def fit(self, X, y):
-        X_b = np.c_[np.ones((X.shape[0], 1)), X]
-        self.coef_ = np.zeros(X_b.shape[1])
+        m, n = X.shape
+        X_b = np.c_[np.ones((m, 1)), X]  # Adding the intercept term
+        self.coef_ = np.zeros(n + 1)
 
         for iteration in range(self.n_iterations):
             linear_model = X_b.dot(self.coef_)
             predictions = self._sigmoid(linear_model)
             
-            # Add regularization term
+            # Gradient with L2 regularization
             regularization_term = self.regularization * np.r_[0, self.coef_[1:]]
-            gradient = (X_b.T.dot(predictions - y) + regularization_term) / X_b.shape[0]
-            
-            prev_coef = np.copy(self.coef_)
+            gradient = (X_b.T.dot(predictions - y) + regularization_term) / m
+
+            # Update weights
             self.coef_ -= self.learning_rate * gradient
 
-            # Check convergence
-            if np.all(np.abs(self.coef_ - prev_coef) < self.tol):
-                print(f'Converged at iteration {iteration}')
+            # Check for convergence
+            if np.linalg.norm(gradient) < self.tol:
                 break
 
         self.intercept_ = self.coef_[0]
         self.coef_ = self.coef_[1:]
 
     def predict_proba(self, X):
-        X_b = np.c_[np.ones((X.shape[0], 1)), X]
-        linear_model = X_b.dot(np.r_[self.intercept_, self.coef_])
-        return self._sigmoid(linear_model)
+        X_b = np.c_[np.ones((X.shape[0], 1)), X]  # Adding the intercept term
+        return self._sigmoid(X_b.dot(np.r_[self.intercept_, self.coef_]))
 
     def predict(self, X):
         return (self.predict_proba(X) >= 0.5).astype(int)
