a5eee3780ab176e35571cd8d5d1f2227508be9c5
Basma-Arnaoui <basma.arnaoui@gmail.com>
2024-05-05 16:09:19 +0100

    new file struct

--- estimators/__init__.py
+++
@@ -1,3 +0,0 @@
-from .base import BaseEstimator
-
-__all__ = ['BaseEstimator']  # Specify which classes to import when using wildcard imports
--- predictors/__init__.py
+++
@@ -1,3 +0,0 @@
-from .base import BasePredictor
-
-__all__ = ['BasePredictor']  # Specify which classes to import when using wildcard imports
